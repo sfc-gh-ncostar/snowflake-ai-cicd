306f9a474e0656dc9331374a7ee04fc6198c254e
Noah Costar <noah.costar@snowflake.com>
2025-10-22 08:45:44 +1300

    Add Semantic View to Agent

--- components/cortex_agent/housekeeping.sql
+++ components/cortex_agent/housekeeping.sql
@@ -31,18 +31,28 @@ FROM SPECIFICATION $$
           "name": "cortex_snowflake_documentation",
           "description": "Allows users to get documentations from Snowflake Documentation."
         }
+      },
+      {
+        "tool_spec": {
+          "type": "cortex_analyst_text_to_sql",
+          "name": "snowflake_account_usage",
+          "description": "This tool allows users to query the Snowflake account usage schema. It provides a unified business layer over the Snowflake ACCOUNT_USAGE schema, designed for platform owners and administrators. The model is optimized for natural language queries with Cortex Analyst, allowing users to proactively manage the environment by asking questions related to cost efficiency, operational performance, and security governance."
+        }
       }
     ],
     "tool_resources": {
         "cortex_search_snowflake_query_history": {
           "id_column": "QUERY_TEXT",
           "max_results": 5,
-          "name": "TECHUP25.{{ENV}}AGENTIC_AI.QUERY_HISTORY_SEARCH_SERVICE",
+          "name": "TECHUP25.{{ENV}}AGENTIC_AI.QUERY_HISTORY_SEARCH_SERVICE"
         },
         "cortex_snowflake_documentation": {
           "id_column": "CHUNK",
           "max_results": 5,
-          "name": "SNOWFLAKE_DOCUMENTATION.SHARED.CKE_SNOWFLAKE_DOCS_SERVICE",
+          "name": "SNOWFLAKE_DOCUMENTATION.SHARED.CKE_SNOWFLAKE_DOCS_SERVICE"
+        },
+        "snowflake_account_usage": {
+          "semantic_view": "TECHUP25.{{ENV}}AGENTIC_AI.SNOWFLAKE_HOUSEKEEPING_AGENT"
         }
       }
 }
